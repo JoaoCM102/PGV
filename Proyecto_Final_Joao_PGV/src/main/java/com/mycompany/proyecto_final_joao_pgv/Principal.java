4b45839af1296f703761ee96ed454bb7690295fb
JoaoCM102 <118294116+JoaoCM102@users.noreply.github.com>
2024-01-29 12:47:32 +0100

    Esto es mio
    a

---
+++ Proyecto_Final_Joao_PGV/src/main/java/com/mycompany/proyecto_final_joao_pgv/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_joao_pgv;
+
+/**
+ *
+ * @author joaop
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
